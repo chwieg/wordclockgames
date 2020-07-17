ad7120698b05d56b5e28f4b8e5512bed7c2b4c7f
Christian <ch.wiegand@web.de>
2020-07-17 07:57:33 +0200

    first working version of game in backend with control by api

--- backend/connectfour.go
+++ backend/connectfour.go
@@ -2,38 +2,47 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"os"
-	"time"
+	//	"math/rand"
+	//"os"
+	//"time"
 )
 
 const nRows = 10
 const nCols = 11
 
-type State int
+type state int
 
 const (
-	stateWinner State = iota
-	stateDraw
-	stateContinue
+	stRunning state = iota
+	stStopWinner
+	stStopDraw
 )
 
-type Game struct {
-	board        [nRows][nCols]int // bord[0][0] is upper left corner
-	allowedMoves []int
-	activePlayer int
-	state        State
+type game struct {
+	Board        [nRows][nCols]int // bord[0][0] is upper left corner
+	AllowedMoves []int
+	ActivePlayer int
+	State        state
 }
 
-func (g *Game) showBoard() {
+func newGame() game {
+	g := game{
+		State:        stRunning,
+		ActivePlayer: 1,
+	}
+	g.updateAllowedMoves()
+	return g
+}
+
+func (g *game) showBoard() {
 	fmt.Println()
 	fmt.Println("Board:")
 	for i := 0; i < nRows; i++ {
 		for j := 0; j < nCols; j++ {
-			if g.board[i][j] == 0 {
+			if g.Board[i][j] == 0 {
 				fmt.Print(".")
 			} else {
-				fmt.Print(g.board[i][j])
+				fmt.Print(g.Board[i][j])
 			}
 			fmt.Print(" ")
 		}
@@ -42,20 +51,20 @@ func (g *Game) showBoard() {
 	fmt.Println()
 }
 
-func (g *Game) updateAllowedMoves() {
-	g.allowedMoves = nil
+func (g *game) updateAllowedMoves() {
+	g.AllowedMoves = nil
 	for i := 0; i < nCols; i++ {
-		if g.board[0][i] == 0 {
-			g.allowedMoves = append(g.allowedMoves, i)
+		if g.Board[0][i] == 0 {
+			g.AllowedMoves = append(g.AllowedMoves, i)
 		}
 	}
 }
 
-func (g *Game) isAllowedMove(col int) bool {
+func (g *game) isAllowedMove(col int) bool {
 	if col < 0 || col >= nCols {
 		return false
 	} else {
-		for _, allowedCol := range g.allowedMoves {
+		for _, allowedCol := range g.AllowedMoves {
 			if allowedCol == col {
 				return true
 			}
@@ -64,26 +73,26 @@ func (g *Game) isAllowedMove(col int) bool {
 	}
 }
 
-func (g *Game) move(col int) {
+func (g *game) move(col int) {
 	for i := nRows - 1; i >= 0; i-- {
-		if g.board[i][col] == 0 {
-			g.board[i][col] = g.activePlayer
+		if g.Board[i][col] == 0 {
+			g.Board[i][col] = g.ActivePlayer
 			g.updateAllowedMoves()
 			break
 		}
 	}
 }
 
-func (g *Game) checkBoard() State {
+func (g *game) checkBoard() state {
 
 	// check vertical
 	for j := 0; j < nCols; j++ {
 		cntFields := 0
 		for i := 0; i < nRows; i++ {
-			if g.board[i][j] == g.activePlayer {
+			if g.Board[i][j] == g.ActivePlayer {
 				cntFields += 1
 				if cntFields == 4 {
-					return stateWinner
+					return stStopWinner
 				}
 			} else {
 				cntFields = 0
@@ -94,10 +103,10 @@ func (g *Game) checkBoard() State {
 	for i := 0; i < nRows; i++ {
 		cntFields := 0
 		for j := 0; j < nCols; j++ {
-			if g.board[i][j] == g.activePlayer {
+			if g.Board[i][j] == g.ActivePlayer {
 				cntFields += 1
 				if cntFields == 4 {
-					return stateWinner
+					return stStopWinner
 				}
 			} else {
 				cntFields = 0
@@ -112,10 +121,10 @@ func (g *Game) checkBoard() State {
 			if i+j < 0 {
 				continue
 			}
-			if g.board[i+j][j] == g.activePlayer {
+			if g.Board[i+j][j] == g.ActivePlayer {
 				cntFields += 1
 				if cntFields == 4 {
-					return stateWinner
+					return stStopWinner
 				}
 			} else {
 				cntFields = 0
@@ -130,10 +139,10 @@ func (g *Game) checkBoard() State {
 			if i-j >= nRows {
 				continue
 			}
-			if g.board[i-j][j] == g.activePlayer {
+			if g.Board[i-j][j] == g.ActivePlayer {
 				cntFields += 1
 				if cntFields == 4 {
-					return stateWinner
+					return stStopWinner
 				}
 			} else {
 				cntFields = 0
@@ -144,65 +153,65 @@ func (g *Game) checkBoard() State {
 	// check if draw
 	for j := 0; j < nCols; j++ {
 		for i := 0; i < nRows; i++ {
-			if g.board[i][j] == 0 {
-				return stateContinue
+			if g.Board[i][j] == 0 {
+				return stRunning
 			}
 		}
 	}
-	return stateDraw
+	return stStopDraw
 
 }
-
+func (g *game) switchActivePlayer() {
+if g.State == stRunning {
+		if g.ActivePlayer == 1 {
+			g.ActivePlayer = 2
+		} else {
+			g.ActivePlayer = 1
+		}
+}
+}
+/*
 func main() {
-
-	g := Game{
-		state:        stateContinue,
-		activePlayer: 1,
-	}
-	g.updateAllowedMoves()
+	g := newGame()
 
 	col := 0
+
 	for i := 0; i < nRows*nCols+10; i++ {
 		/*
 			// simple vertical strategy
-			if i==2 && g.activePlayer==1 {
+			if i==2 && g.ActivePlayer==1 {
 				col = 3 // fatal mistakte by player 1
 			} else {
-				col = g.activePlayer
+				col = g.ActivePlayer
 			}
-		*/
+		* /
 		// random Strategy
 		src := rand.NewSource(time.Now().UnixNano())
 		randSeeded := rand.New(src)
-		col = g.allowedMoves[randSeeded.Intn(len(g.allowedMoves))]
+		col = g.AllowedMoves[randSeeded.Intn(len(g.AllowedMoves))]
 
 		if !g.isAllowedMove(col) {
 			fmt.Println("Invalid move")
 			os.Exit(-1)
 		}
 		g.move(col)
-		g.state = g.checkBoard()
-		switch g.state {
-		case stateWinner:
+		g.State = g.checkBoard()
+		switch g.State {
+		case stStopWinner:
 			fmt.Println()
-			fmt.Println("Winner: Player ", g.activePlayer)
+			fmt.Println("Winner: Player ", g.ActivePlayer)
 			g.showBoard()
-			os.Exit(g.activePlayer)
-		case stateDraw:
+			os.Exit(g.ActivePlayer)
+		case stStopDraw:
 			fmt.Println()
 			fmt.Println("No winner.")
 			g.showBoard()
 			os.Exit(0)
 		}
-		if g.activePlayer == 1 {
-			g.activePlayer = 2
-		} else {
-			g.activePlayer = 1
-		}
-
 		// simple horizontal strategy
-		if g.activePlayer == 1 {
+		if g.ActivePlayer == 1 {
 			col += 1
 		}
 	}
 }
+*/
--- backend/server.go
+++ backend/server.go
@@ -6,26 +6,81 @@ import (
 	"net/http"
 )
 
-type dummyGame struct {
-	Name         string
-	State        int
-	ActivePlayer int
+type clientData struct {
+	Column int
 }
 
-func connectFourHandler(w http.ResponseWriter, r *http.Request) {
-	game := dummyGame{"test", 0, 1}
-	json, err := json.Marshal(game)
-/* print for debugging
-	fmt.Println(game)
-	fmt.Println("err:  ", err)
-	fmt.Println("json: ", json)
-*/
+var g game
+
+func setupResponse(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	//  (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, PUT")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func conFourNewGameHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	g = newGame()
+	json, _ := json.Marshal(g)
 	w.Write(json)
+}
+func conFourHandler(w http.ResponseWriter, r *http.Request) {
+	setupResponse(&w, r)
+	fmt.Println("Request Method: ", r.Method)
+	if (*r).Method == "OPTIONS" {
+		return
+	}
+	switch r.Method {
+	case http.MethodGet:	// Serve the resource.
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json, _ := json.Marshal(g)
+		w.Write(json)
+	case http.MethodPut:	// Update an existing record.
+		w.Header().Set("Content-Type", "application/json")
+
+		var d clientData
+		err := json.NewDecoder(r.Body).Decode(&d)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		if g.State != stRunning {
+			http.Error(w, "Game is already over", http.StatusBadRequest)
+			return
+		}
+
+		if !g.isAllowedMove(d.Column) {
+			http.Error(w, "Invalid value for Column", http.StatusBadRequest)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+
+		g.move(d.Column)
+		g.State = g.checkBoard()
+		g.switchActivePlayer()
+
+		json, _ := json.Marshal(g)
+		w.Write(json)
+	}
+	/*	// debugging
+		json, err := json.Marshal(g)
+		fmt.Println(g)
+		fmt.Println("err:  ", err)
+		fmt.Println("json: ", json)
+	*/
 }
 
 func main() {
-
-	http.HandleFunc("/wordclock/connectfour", connectFourHandler)
+	g = newGame()
+	g.showBoard()
+	http.HandleFunc("/wordclock/connectfour", conFourHandler)
+	http.HandleFunc("/wordclock/connectfour/newgame", conFourNewGameHandler)
 	http.ListenAndServe(":8080", nil)
 
 }
