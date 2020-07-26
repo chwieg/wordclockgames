f51922792e165b2b7b39d0e067b026d8b46f073f
Christian <ch.wiegand@web.de>
2020-07-27 00:29:37 +0200

    Erfolgreicher Build und Flashen mit PlatformIO
    - library dependency hinzugefügt
    - board auf Uno umgestellt, damit ist flashen möglich

---
+++ firmware/PlatformIO/Qlockthree/main.cpp
@@ -0,0 +1,16 @@
+/*
+  wurde von PlatformIO angelegt als default für Arduino-Projekte.
+  Bei Qlockthree aus GitHub war schon eine .ino dabei,
+  deshalb diese Datei nur als Vorlage außerhalb von src aufgehoben
+
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
