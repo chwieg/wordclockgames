1a81d73b63f2f810c816a4c06972ed2ec054ba8e
Christian <ch.wiegand@web.de>
2020-08-05 23:40:49 +0200

    Prototypische Ansteuerung über BT aus Backend ok
    Umsetzung in bluetooth_proto.go

---
+++ backend/bluetooth_proto.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os/exec"
+	"strconv"
+)
+
+// converts input hex str to str of hex ascii codes representing the decimal value of the hex input
+func hex2decAscii(hexStr string) string {
+
+	fmt.Printf("Input: %v\n", hexStr)
+
+	decInt, _ := strconv.ParseUint(hexStr, 0, 64)
+	fmt.Printf("%T, %v\n", decInt, decInt)
+
+	decStr := strconv.Itoa(int(decInt))
+	fmt.Printf("%T, %v\n", decStr, decStr)
+
+	decAsciiBytes := []byte(decStr)
+	fmt.Printf("%T, %v\n", decAsciiBytes, decAsciiBytes)
+
+	decAsciiStr := hex.EncodeToString(decAsciiBytes)
+	fmt.Printf("%T, %v\n", decAsciiStr, decAsciiStr)
+
+	return decAsciiStr
+}
+
+func main() {
+	// hex2decAscii("0xff")
+	gattCmd := "sudo gatttool -i hci0 -b 00:13:AA:00:BC:8C --char-write-req -a 0x0025 -n "+
+		//hex2decAscii("0xFF906F") // red
+		hex2decAscii("0xFF609F") // seconds
+
+	fmt.Println("\ncommand to execute:\n")
+	fmt.Println(gattCmd)
+
+	// cmd := exec.Command("/bin/sh", "-c", "sudo find . -name \"*.go\"")
+	cmd := exec.Command("/bin/sh", "-c", gattCmd)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("cmd.Run() failed with %s\n", err)
+	}
+	fmt.Printf("combined out:\n%s\n", string(out))
+}
